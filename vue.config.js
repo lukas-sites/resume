9fa5380838395f1964b02ca96e5d04e2f70c6c58
Lukas <lherman.cs@gmail.com>
2019-07-30 10:50:59 -0700

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
+module.exports = {
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      config.plugins.push(
+        new PrerenderSPAPlugin({
+          // Required - The path to the webpack-outputted app to prerender.
+          staticDir: path.join(__dirname, "dist"),
+          // Required - Routes to render.
+          routes: ["/"]
+        })
+      );
+    }
+  }
+};
