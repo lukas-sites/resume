719650fc42fe46584a78785705236d4597ad0861
Lukas <lherman.cs@gmail.com>
2019-07-30 14:59:30 -0700

    Added basic header and custom font

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <div>
+      <h1>Lukas Herman</h1>
+      <h2>Full Stack Developer</h2>
+      <p>
+        Passionate about coding. Write technical blog. Maintain server. Enjoy
+        being a full stack developer and learning new things every day. Expert
+        in Go, Python, Angular, Linux, and Docker. Happy to relocate.
+      </p>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header:first-child {
+  width: 50%;
+}
+</style>
---
+++ src/components/Resume.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="paper">
+    <my-header></my-header>
+  </div>
+</template>
+
+<script>
+import MyHeader from "./Header.vue";
+
+export default {
+  components: {
+    MyHeader
+  }
+};
+</script>
+
+<style scoped>
+.paper {
+  display: flex;
+  flex-direction: column;
+  width: 210mm;
+  height: 297mm;
+  border: 1px solid black;
+  margin: 0 auto;
+}
+</style>
