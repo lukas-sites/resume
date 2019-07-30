4ad89d2d15c4a6f8e9666ee88e95d982a4aba0e0
Lukas <lherman.cs@gmail.com>
2019-07-30 16:50:10 -0700

    Added default spacing for h1, h2, p

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,26 @@ export default {
 
 <style>
 body {
-  font-family: "Muli", sans-serif;
+  font-family: "Ubuntu", sans-serif;
+  font-size: 70%;
+}
+
+h1 {
+  margin-block-start: 0.1em;
+  margin-block-end: 0.1em;
+}
+
+h2 {
+  margin-block-start: 0.15em;
+  margin-block-end: 0.15em;
+}
+
+p {
+  margin-block-start: 0.5em;
+  margin-block-end: 0.5em;
+}
+
+.flex-spacer {
+  flex-grow: 1;
 }
 </style>
