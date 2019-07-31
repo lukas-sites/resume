51e3f5d07ed90a8dcb1fb3db6d18f8d847ec8fca
Lukas <lherman.cs@gmail.com>
2019-07-30 20:34:09 -0700

    Fixed padding problem in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,29 @@
 <template>
   <header>
-    <div>
-      <h1>Lukas Herman</h1>
-      <h2>Full Stack Developer</h2>
-      <p>
-        Passionate about coding. Write technical blog. Maintain server. Enjoy
-        being a full stack developer and learning new things every day. Expert
-        in Go, Python, Angular, Linux, and Docker. Happy to relocate.
-      </p>
+    <div class="content">
+      <div class="content__wrapper">
+        <h1>Lukas Herman</h1>
+        <h2>Full Stack Developer</h2>
+        <p>
+          Passionate about coding. Write technical blog. Maintain server. Enjoy
+          being a full stack developer and learning new things every day. Expert
+          in Go, Python, Angular, Linux, and Docker. Happy to relocate.
+        </p>
+      </div>
     </div>
   </header>
 </template>
 
 <style scoped>
-header:first-child {
+header {
+  display: flex;
+}
+
+.content {
   width: 50%;
+}
+
+.content__wrapper {
+  padding: 3mm 7mm 1mm;
 }
 </style>
