b611e678a5d380db6b55f9a6c6a993724f83d8b3
Lukas <lherman.cs@gmail.com>
2019-08-04 15:07:22 -0700

    Changed deployment to be in gh-pages branch

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const PrerenderSPAPlugin = require("prerender-spa-plugin");
 
 module.exports = {
   publicPath: "./",
-  outputDir: process.env.NODE_ENV === "production" ? "docs" : "dist",
   configureWebpack: config => {
     if (process.env.NODE_ENV === "production") {
       config.plugins.push(
